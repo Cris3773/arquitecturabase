76af259de66ea098f060dc1ea4fefef970846f4b
Cris3773 <cristilou965@gmail.com>
2025-10-21 19:23:11 +0200

    arreglo bug boton añadirUsuario 2.0

--- index.js
+++ index.js
@@ -7,46 +7,27 @@ const PORT = process.env.PORT || 3000;
 
 let sistema = new modelo.Sistema();
 
-// === Servir archivos estáticos del cliente ===
-// Esto permite acceder a: /cliente/clienteRest.js, /cliente/controlWeb.js, etc.
-app.use(express.static(path.join(__dirname, "cliente")));
 
-// === Ruta raíz ===
+app.use("/cliente", express.static(path.join(__dirname, "cliente")));
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "cliente", "index.html"));
 });
 
-// === RUTAS REST ===
+
 app.get("/agregarUsuario/:nick", (req, res) => {
-  const nick = req.params.nick;
-  const r = sistema.agregarUsuario(nick);
-  console.log("agregarUsuario", nick, "->", r);
+  const r = sistema.agregarUsuario(req.params.nick);
   res.json(r);
 });
-
-app.get("/obtenerUsuarios", (req, res) => {
-  res.json(sistema.obtenerUsuarios());
-});
-
-app.get("/usuarioActivo/:nick", (req, res) => {
-  const nick = req.params.nick;
-  res.json({ nick, activo: sistema.usuarioActivo(nick) });
-});
-
-app.get("/numeroUsuarios", (req, res) => {
-  res.json({ num: sistema.numeroUsuarios() });
-});
-
+app.get("/obtenerUsuarios", (req, res) => res.json(sistema.obtenerUsuarios()));
+app.get("/usuarioActivo/:nick", (req, res) =>
+  res.json({ nick: req.params.nick, activo: sistema.usuarioActivo(req.params.nick) })
+);
+app.get("/numeroUsuarios", (req, res) => res.json({ num: sistema.numeroUsuarios() }));
 app.get("/eliminarUsuario/:nick", (req, res) => {
-  const nick = req.params.nick;
-  sistema.eliminarUsuario(nick);
-  res.json({ nick, eliminado: true });
+  sistema.eliminarUsuario(req.params.nick);
+  res.json({ nick: req.params.nick, eliminado: true });
 });
 
-// === Arranque del servidor ===
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-
-
-
+app.listen(PORT, "0.0.0.0", () => console.log(`Listening on ${PORT}`));
