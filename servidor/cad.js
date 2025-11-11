ecc78f74731030bfe112913b82827815e48dd081
Cris3773 <85738205+Cris3773@users.noreply.github.com>
2025-11-11 18:59:13 +0100

    actualizo cad

--- servidor/cad.js
+++ servidor/cad.js
@@ -5,11 +5,26 @@ function CAD(){
     this.conectar=async function(callback){ 
 let cad=this; 
 let client= new 
-mongo("mongodb+srv://cris3773:1234@cluster0.sqrzlk8.mongodb.net/?appName=Cluster0"); 
+mongo(process.env.mongo_uri);
 await client.connect(); 
 const database=client.db("sistema"); 
 cad.usuarios=database.collection("usuarios"); 
 callback(database); 
 }
+} 
+this.buscarOCrearUsuario=function(usr,callback){ 
+buscarOCrear(this.usuarios,usr,callback); 
+} 
+function buscarOCrear(coleccion,criterio,callback) 
+{ 
+coleccion.findOneAndUpdate(criterio, {$set: criterio}, {upsert: 
+true,returnDocument:"after",projection:{email:1}}, function(err,doc) { 
+if (err) { throw err; } 
+else {  
+console.log("Elemento actualizado");  
+console.log(doc.value.email); 
+callback({email:doc.value.email}); 
+} 
+});   
 } 
 module.exports.CAD=CAD; 
