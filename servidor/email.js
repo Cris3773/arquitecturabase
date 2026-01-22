4b0e26ba703074145e9ffe5e493c59fa45525f7b
Cris3773 <cristilou965@gmail.com>
2026-01-22 22:08:27 +0100

    error enlace confirmar cuenta1.2

--- servidor/email.js
+++ servidor/email.js
@@ -29,9 +29,10 @@ module.exports.enviarEmail = async function (direccion, key, men) {
         text: 'Pulsa aquí para confirmar cuenta',
         html: `
             <p>Bienvenido a Sistema</p>
-            <p><a href="${url}confirmarUsuario/${direccion}/${key}">
+            <a href="${url}/confirmarUsuario/${direccion}/${key}">
+
                 Pulsa aquí para confirmar cuenta
-            </a></p>
+            </a>
         `
     });
 
